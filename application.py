d5defed45f244ed607057ce6b92e02e164431176
Pranoy Dev <devpranoy@gmail.com>
2021-08-03 18:12:12 +0530

    Final Build

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask.helpers import url_for
-
+import os
+import subprocess
 application = app = Flask(__name__, template_folder="templates/", static_url_path='/')
 app.config['UPLOAD_FOLDER'] = "uploads"
 
@@ -8,9 +9,27 @@ app.secret_key = 'some_random_key'
 
 
 
-@app.route("/", methods=['get'])
+@app.route("/", methods=['GET','POST'])
 def home():
-        return render_template("index.html")
+    if request.method == 'POST':
+        address = request.form['address']
+        if len(address)!=44:
+            flash('Please Enter a valid Solana Address','danger')
+            return render_template("index.html")
+        render_template('loading.html')
+        processd = subprocess.run(['solana', 'airdrop', '1'  ,address, '--url', 'https://api.devnet.solana.com'], 
+                         stdout=subprocess.PIPE, 
+                         universal_newlines=True)
+        processt = subprocess.run(['solana', 'airdrop', '1'  ,address, '--url', 'https://api.testnet.solana.com'], 
+                         stdout=subprocess.PIPE, 
+                         universal_newlines=True)
+        
+        print(processd, processt)
+
+        flash('SOL has been sent successfully','success')
+
+
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True)
